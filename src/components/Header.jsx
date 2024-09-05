f400cb7ee2f0db6dfc7f35507bb1a51a1bebfdc7
kachraz <kachraz@tuta.io>
2024-09-05 16:02:56 +0530

    TextEdits

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
-import logo from '../assets/logo.png';
+import logo from "../assets/logo.png";
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <h1>PantySmell</h1>
+      <p>A community of AssAndPussy Lickers.</p>
     </header>
   );
 }
